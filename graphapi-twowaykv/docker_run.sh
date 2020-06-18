efe224827593a26c14f06b4d4a5609f46fd003cb
dgoldstein1 <david.goldstein@deciphernow.com>
2020-06-18 19:38:12 -0400

    able to start graphapi

--- graphapi-twowaykv/docker_run.sh
+++ graphapi-twowaykv/docker_run.sh
@@ -5,4 +5,22 @@
 # - https://github.com/dgoldstein1/graphapi on 5000
 # - https://github.com/dgoldstein1/reverse-proxy on $PORT
 
-echo "GOT HERE"
+# init app configs
+init() {
+	touch logs.txt
+}
+
+# runs graphapi
+start_graphapi() {
+	cd /urs/graphapi
+	export GRAPH_SAVE_PATH=/data/graphapi/current_graph.graph
+	flask run --host=0.0.0.0 --port 5000
+}
+
+# start jobs
+init
+start_graphapi > logs.txt &
+
+printenv
+tail -f logs.txt
+
