6c4e63ac90cbb12a93b6b2b3bd06fa3fb7f88c08
dgoldstein1 <david.goldstein@deciphernow.com>
2020-06-18 19:24:05 -0400

    initial setup

---
+++ graphapi-twowaykv/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -euf -o pipefail
+
+
+# build image
+
+docker build . \
+ -t dgoldstein1/links-heroku-graphapi-twowaykv:latest
+
+ 
---
+++ graphapi-twowaykv/docker_run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# run all three
+# - https://github.com/dgoldstein1/twowaykv on 5001
+# - https://github.com/dgoldstein1/graphapi on 5000
+# - https://github.com/dgoldstein1/reverse-proxy on $PORT
+
+echo "GOT HERE"
