de57fc970231837eebdab6850823adc332b69209
dgoldstein1 <david.goldstein@deciphernow.com>
2020-06-18 20:26:45 -0400

    everything is running

--- graphapi-twowaykv/docker_run.sh
+++ graphapi-twowaykv/docker_run.sh
@@ -18,12 +18,26 @@ start_graphapi() {
 	cd /usr/graphapi
 	mkdir -p /data/graphapi/
 	flask run --host=0.0.0.0 --port 5000
+	fail "graphapi"
 }
 
 start_twowaykv() {
-	cd /usr/twowaykv
 	mkdir -p /data/twowaykv/
 	/usr/twowaykv/twowaykv serve
+	fail "twowaykv"
+}
+
+start_reverseproxy() {
+	/usr/reverseproxy/reverseproxy
+	fail "reverseproxy"
+}
+
+# service has failed
+# $1 = service name
+fail() {
+	echo "-------------------------------" >> logs.txt
+	echo "------------ $1 failure -------" >> logs.txt
+	echo "-------------------------------" >> logs.txt
 }
 
 # graphapi
@@ -32,10 +46,12 @@ export GRAPH_SAVE_PATH=/data/graphapi/current_graph.graph
 export GRAPH_DOCS_DIR=/usr/twowaykv/docs/*
 export GRAPH_DB_STORE_DIR=/data/twowaykv
 
+
 # start jobs
 init
 start_graphapi > logs.txt &
 start_twowaykv > logs.txt &
+start_reverseproxy > logs.txt &
 
 printenv
 tail -f logs.txt
