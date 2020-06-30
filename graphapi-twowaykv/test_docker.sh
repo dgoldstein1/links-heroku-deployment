143e5fecc2ca7c2f03465740b249900ed577b267
dgoldstein1 <david.goldstein@deciphernow.com>
2020-06-30 19:18:07 -0400

    twowaykv syncing, graph api is failing

--- graphapi-twowaykv/test_docker.sh
+++ graphapi-twowaykv/test_docker.sh
@@ -5,7 +5,7 @@
 docker run \
 	-e AWS_ACCESS_KEY_ID \
 	-e AWS_SECRET_ACCESS_KEY \
-	-e READ_S3=true \
+	-e READ_S3=false \
 	-e WRITE_S3=true \
 	-e AWS_SYNC_DIRECTORY="s3://links-deployment-dev/local-testing/" \
 	-p 5000:5000 \
